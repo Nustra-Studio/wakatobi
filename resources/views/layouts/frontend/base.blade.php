2eeb52acf69430c0355324830569e714936f5b2f
manggala56 <manggalaputra740@gmail.com>
2023-07-24 23:03:40 +0700

    navbar js

--- resources/views/layouts/frontend/base.blade.php
+++ resources/views/layouts/frontend/base.blade.php
@@ -153,7 +153,7 @@
 </footer> --}}
 <script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
 <script src="{{asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 @yield('customjs')
 @yield('customscripts')
 </html>
