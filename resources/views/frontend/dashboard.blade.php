d46bdc358defda375d6883e109f70f0ed7644a08
manggala56 <manggalaputra740@gmail.com>
2023-07-29 10:17:38 +0700

    coursel

--- resources/views/frontend/dashboard.blade.php
+++ resources/views/frontend/dashboard.blade.php
@@ -112,39 +112,35 @@
             </div>
         </div> --}}
         <!-- section 1 -->
-        <div class="row">
-          <div class="col-12">
-            <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-              <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="10000">
-                  <img src="img/Section1BG.png" class="d-block w-100" alt="...">
-                  <style>
-                    .carousel-caption {
-                          top: 30%;
-                          bottom: auto;
-                      }
-                  </style>
-                  <div class="carousel-caption d-flex justify-content-center ">
-                    <div class="text-center col-8">
-                      <h1 class="text-white">Menghubungkan, Mengamankan, dan Mengelola Data Anda!</h1>
-                      <p class="text-white">Membantu anda mengelola data dengan mudah, termasuk pengumpulan,
-                          penyimpanan dan pengorganisasian data.</p>    
-                </div>
-              </div>
-                </div>
-              </div>
-              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-              </button>
+        <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="10000">
+              <img src="img/Section1BG.png" class="d-block w-100" alt="...">
+              <style>
+                .carousel-caption {
+                      top: 30%;
+                      bottom: auto;
+                  }
+              </style>
+              <div class="carousel-caption d-flex justify-content-center ">
+                <div class="text-center col-8">
+                  <h1 class="text-white">Menghubungkan, Mengamankan, dan Mengelola Data Anda!</h1>
+                  <p class="text-white">Membantu anda mengelola data dengan mudah, termasuk pengumpulan,
+                      penyimpanan dan pengorganisasian data.</p>    
             </div>
           </div>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
-        
+
     <!-- section 2 -->
         <section class="container section-2">
             <div class="d-flex section-2-container">
