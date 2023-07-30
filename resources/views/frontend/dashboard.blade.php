236c14cb8b6a8dfcb30db00448ce7b06769df33f
manggala56 <manggalaputra740@gmail.com>
2023-07-31 01:39:12 +0700

    berita

--- resources/views/frontend/dashboard.blade.php
+++ resources/views/frontend/dashboard.blade.php
@@ -464,7 +464,7 @@
                             <div class=" galeri-item d-none"><img src="{{asset('images/galeri/'.$galeri->gambar)}}" alt=""></div>
                             @elseif ($i >= 1)
                                 <div class="card card-galeri mb-4">
-                                    <img src="{{asset('images/galeri/'.$galeri->gambar)}}" class="card-img img-galeri" width="530" height="265" alt="...">
+                                    <img src="{{asset('images/galeri/'.$galeri->gambar)}}" class="card-img img-galeri" width="530" height="263" alt="...">
                                 </div>
                             @endif
                             @php $i++; @endphp
