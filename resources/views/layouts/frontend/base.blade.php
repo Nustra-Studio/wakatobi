60e1578bb5131fea5272200b6f997cc5c18e3a49
manggala56 <manggalaputra740@gmail.com>
2023-07-31 23:55:30 +0700

    home page

--- resources/views/layouts/frontend/base.blade.php
+++ resources/views/layouts/frontend/base.blade.php
@@ -11,11 +11,19 @@
   <title>{{$ws->singkatan}}</title>
   @endforeach
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <link rel="stylesheet" href="{{asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
   {{-- <link rel="stylesheet" href="{{asset('assets/dist/css/adminlte.min.css')}}">
   <link rel="stylesheet" href="{{asset('assets/dist/css/custom.css')}}"> --}}
   <link rel="stylesheet" href="{{asset('assets/style.css')}}">
+  <style>
+    body {
+      font-family: 'Poppins', sans-serif;
+    }
+  </style>
   {{-- <style>
     .imgcustom {
       height: 250px;
