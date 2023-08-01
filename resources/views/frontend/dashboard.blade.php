5990dae9c9ddf9a28a1bb69a660ecebf4310bbef
manggala56 <manggalaputra740@gmail.com>
2023-08-02 06:14:37 +0700

    tentang kami

--- resources/views/frontend/dashboard.blade.php
+++ resources/views/frontend/dashboard.blade.php
@@ -494,14 +494,25 @@
                     </div>
                 </div> --}}
                 <div class="row mt-3">
+                    @php
+                        $i=0;
+                    @endphp
                     @foreach ($data3 as $galeri)
                     <div class="col-lg-6">
+                        @if ($i == 0)
                         <img src="{{asset('images/galeri/'.$galeri->gambar)}}" class="card-img" alt="...">
+                        @endif
+                        @if ($i == 1)
                         <img src="{{asset('images/galeri/'.$galeri->gambar)}}" class="card-img" alt="...">
+                        @endif
                     </div>
                     <div class="col-lg-6">
+                        @if ($i == 2)
                         <img src="{{asset('images/galeri/'.$galeri->gambar)}}" class="card-img" alt="...">
+                        @endif
+                        @if ($i == 3)
                         <img src="{{asset('images/galeri/'.$galeri->gambar)}}" class="card-img" alt="...">
+                        @endif
                     </div>
                                         
                     @endforeach
