751530c1b2b8f19061f2b3ba9407298991823c31
manggala56 <manggalaputra740@gmail.com>
2023-08-03 22:14:20 +0700

    galeri

--- resources/views/frontend/dashboard.blade.php
+++ resources/views/frontend/dashboard.blade.php
@@ -461,21 +461,19 @@
                         @foreach($data3 as $galeri)
                             @if ($i % 2 == 0 )
                             <div class=" col-lg-4 col-md-4 galeri-items">
-                                @if ($i % 2 != 0 )
                                 <div class="card card-galeri galeri-item">
                                     <img src="{{asset('images/galeri/'.$galeri->gambar)}}" class="card-img" alt="...">
                                     <div class="card-img-overlay card-container-galeri">
                                     <h5 class="card-title">PENANDATANGANAN KOMITMEN DEWAN SMART CITY KABUPATEN WAKATOBI</h5>
+                                    </div>
                                 </div>
-                                </div>
-                                @else
                                 <div class="card card-galeri galeri-item">
                                     <img src="{{asset('images/galeri/'.$galeri->gambar)}}" class="card-img" alt="...">
                                     <div class="card-img-overlay card-container-galeri">
                                     <h5 class="card-title">PENANDATANGANAN KOMITMEN DEWAN SMART CITY KABUPATEN WAKATOBI</h5>
+                                    </div>
                                 </div>
-                                </div>
-                                @endif
+                                
                     </div>
                     @endif
                     @php $i++; @endphp
